197d15b3d6a68d768da5d15e542e18b4dc51a089
TarushGupta12345 <tarushgs@gmail.com>
2025-12-20 00:02:54 -0800

    bpe tokenizer first pass

---
+++ src/model/transformer.py
@@ -0,0 +1,25 @@
+import numpy as np
+from tokenizer import Tokenizer
+
+class Transformer:
+    def __init__(self):
+        self.tokenizer = Tokenizer()
+        
+        #running training loop for tokenizer
+        self.tokenizer.tokenize_train()
+
+        
+
+
+
+    def forward(self):
+
+
+
+
+
+class AttentionBlock:
+
+class LayerNorm:
+
+class FFN:
--- src/tokenizer/tokenizer.py
+++ src/tokenizer/tokenizer.py
@@ -11,6 +11,7 @@ class Tokenizer:
         with open(os.path.join(PROJECT_ROOT, "data/processed/meditations.txt"), "r") as f:
             self.meditations = f.read()
         self.vocab_length = vocab_length
+        self.d_model = d_model
 
     def characterize(self, text, protected = None):
         newline = r"[\n]"        
@@ -35,15 +36,15 @@ class Tokenizer:
 
         vocab = list(set(characters))
         vocab.extend(self.protected_tokens)
-
-
+        
+        self.rules = []
         while len(vocab) < self.vocab_length:
             
             a = 0
             pairs = []
 
             while a < len(characters) - 1:
-                if a < characters[a:].index("</w>"):
+                if not characters[a+1] == "</w>":
                     pair = (characters[a], characters[a+1])
                     pairs.append(pair)
                     a += 1
@@ -65,19 +66,73 @@ class Tokenizer:
             i = 0
             characters2 = []
             while i < len(characters):
-                flag = False
-                for j in range(len(rule)):
-                    if i+j >= len(characters) or not characters[i+j] == rule[j]:
-                        flag = True
-                        characters2.append(characters[i])
-                        i += 1
-                        break
-                if flag:
-                    continue
-                characters2.append(rulestr)
-                i += len(rule)
+                if i < len(characters) - 1 and characters[i] == rule[0][0] and characters[i+1] == rule[0][1]:
+                    merged = rule[0][0] + rule[0][1]
+                    characters2.append(merged)
+                    i += 2
+                else:    
+                    characters2.append(characters[i])
+                    i += 1
+
 
             characters = characters2
+
+            self.rules.append((rule[0]))
+
+        self.vocab = vocab
+        
+    def tokenize_train(self):
+        self.bpe_train()
+        tokens_to_ids = {}
+        ids_to_tokens = {}
+        for i in range(len(self.vocab)):
+            tokens_to_ids[self.vocab[i]] =  i
+            ids_to_tokens[i] = self.vocab[i]
+
+        with open('data/vocabulary/tokens_to_ids.json', 'w') as file:
+            json.dump(tokens_to_ids, file) 
+        self.tokens_to_ids = tokens_to_ids
+
+        with open('data/vocabulary/ids_to_tokens.json', 'w') as file:
+            json.dump(ids_to_tokens, file)
+        self.ids_to_tokens = ids_to_tokens
+
+        if os.path.exists('data/weights/embeddings.npy'):
+            E = np.load('data/weights/embeddings.npy')
+        else:
+            E = np.random.normal(0, 0.02, (self.vocab_length, self.d_model))
+        self.E = E
+
+        if not os.path.exists("data/vocabulary/embeddings.npy"):
+            np.save("data/vocabulary/embeddings.npy", E)
+        
+        with open("data/vocabulary/rules.json", "w") as f:
+            json.dump(self.rules, f)
+
+        
+    def encode(self, input_str):
+        tokens = self.characterize(input_str, self.protected_tokens)
+
+        i = 0
+        
+        for rule in self.rules:
+            new_tokens = []
+            i = 0
+            while i < len(tokens):
+                if i < len(tokens) - 1 and tokens[i] == rule[0] and tokens[i+1] == rule[1]:
+                    merged = tokens[i] + tokens[i+1]
+                    new_tokens.append(merged)
+                    i += 2
+                else:
+                    new_tokens.append(tokens[i])
+                    i += 1
+
+            tokens = new_tokens
+
         
 
-    def tokenize():
+        return [self.tokens_to_ids[token] for token in tokens]
+
+
+    def decode(self, input_nums):
+        return [self.ids_to_tokens[int(i)] for i in input_nums]
